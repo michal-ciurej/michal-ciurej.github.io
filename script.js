2a9f84208251af29164b1820d3c30772d07df62b
Michal Ciurej <67523245+michal-ciurej@users.noreply.github.com>
2020-12-02 21:53:12 +0000

    last touches

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-window.onload = function () {
-    document
-      .querySelector(".say-hi-button")
-      .addEventListener("click", function () {
-        window.location.href="tel:0044 7795522340";
-      });
-
-  };
 
   AFRAME.registerComponent('markerhandler', {
 init: function () {
   this.el.sceneEl.addEventListener('markerFound', () => {
-    console.log("doesthiscall");
+    console.log("Caught Marker external");
 
     document.getElementById("bro").style.visibility = "hidden";
-  })
+    document.getElementById("title").style.visibility = "visible";
+    document.getElementById("sub").style.visibility = "visible";
+    document.getElementById("subsub").style.visibility= "visible";
+    document.getElementById("callme").style.background= "linear-gradient(to bottom, #cc9933, #cc3300)";
+          })
   this.el.sceneEl.addEventListener('markerLost', () => {
-    console.log("call2");
+    console.log("Lost Marker external");
 
     document.getElementById("bro").style.visibility = "visible";
   })
