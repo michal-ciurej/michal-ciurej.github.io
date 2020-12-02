80a147b68795c6866d39e7c9d0127c69d65f1649
Michal Ciurej <67523245+michal-ciurej@users.noreply.github.com>
2020-12-02 13:52:17 +0000

    Responsiveness works but broke functionality
    How does CSS alone manage to interfere with Javascript? I do not know, but apparently it does. Debugging ensue.

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
+console.log("loadingJS")
 window.onload = function () {
+  console.log("hi")
     document
-      .querySelector(".say-hi-button")
+      .querySelector(".callButton")
       .addEventListener("click", function () {
+        console.log("fuuuuuuuuuuuuuuuuuuuuuuuuuuuuck")
         window.location.href="tel:0044 7795522340";
       });
 
   };
-
-  AFRAME.registerComponent('markerhandler', {
-init: function () {
-  this.el.sceneEl.addEventListener('markerFound', () => {
-    console.log("doesthiscall");
-
-    document.getElementById("bro").style.visibility = "hidden";
-  })
-  this.el.sceneEl.addEventListener('markerLost', () => {
-    console.log("call2");
-
-    document.getElementById("bro").style.visibility = "visible";
-  })
-}
-});
