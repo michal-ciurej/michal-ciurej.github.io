f474483924a6305e4069a9bddc765e2d8f7a4923
Michal Ciurej <67523245+michal-ciurej@users.noreply.github.com>
2020-12-03 19:15:19 +0000

    Prompt + cleanup
    sensible variable names and prompt on start

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ init: function () {
   this.el.sceneEl.addEventListener('markerFound', () => {
     console.log("Caught Marker external");
 
-    document.getElementById("bro").style.visibility = "hidden";
+    document.getElementById("marker").style.visibility = "hidden";
+    document.getElementById("prompt").style.visibility = "hidden"
     document.getElementById("title").style.visibility = "visible";
     document.getElementById("sub").style.visibility = "visible";
     document.getElementById("subsub").style.visibility= "visible";
     document.getElementById("box").classList.add('moved');
-    // document.getElementById("box").style.width="75%";
           })
   this.el.sceneEl.addEventListener('markerLost', () => {
     console.log("Lost Marker external");
 
-    document.getElementById("bro").style.visibility = "visible";
+    document.getElementById("marker").style.visibility = "visible";
   })
 }
 });
